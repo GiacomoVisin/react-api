afd5cbaa73eb44312eb70c46d7bfc4123a21c516
GiacomoVisin <killerboss1150@gmail.com>
2026-05-09 02:30:43 +0200

    first fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,38 @@
 import { useState,useEffect } from "react"
 
+
+
 function App() {
+
+const [actress,setActress] = useState([])
+const api = "https://lanciweb.github.io/demo/api/actresses/"
+
+
+useEffect(()=>{
+  console.log("useEffect Test");
+
+  fetch(api)
+  .then(res => res.json())
+  .then(actress => {
+    console.log(actress);
+    setActress(actress)
+    
+  })
+},[])
+
+
 
 
   return (
     <>
-    
+     <h1> Actors and Actress list</h1>
+     <div>
+      <ul>
+        {actress.map((actress)=>(
+          <li key={actress.id}>{actress.name}</li>
+        ))}
+      </ul>
+     </div>
     </>
   )
 }
