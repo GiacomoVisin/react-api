e65b3de35c6434a3b4521de185ad114e3d61c0a1
GiacomoVisin <killerboss1150@gmail.com>
2026-05-09 02:36:37 +0200

    milestone 1 completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,51 @@
-import { useState,useEffect } from "react"
+import { useState, useEffect } from "react"
 
 
 
 function App() {
 
-const [actress,setActress] = useState([])
-const api = "https://lanciweb.github.io/demo/api/actresses/"
+  const [actress, setActress] = useState([])
+  const [actors, setActors] = useState([])
+  const api = "https://lanciweb.github.io/demo/api/actresses/"
+  const api2 = "https://lanciweb.github.io/demo/api/actors/"
 
 
-useEffect(()=>{
-  console.log("useEffect Test");
+  useEffect(() => {
+    console.log("useEffect Test");
 
-  fetch(api)
-  .then(res => res.json())
-  .then(actress => {
-    console.log(actress);
-    setActress(actress)
-    
-  })
-},[])
+    fetch(api)
+      .then(res => res.json())
+      .then(actress => {
+        console.log(actress);
+        setActress(actress)
+
+      })
+  }, [])
+
+
+  useEffect(() => {
+    fetch(api2)
+      .then(res => res.json())
+      .then(actress => {
+        console.log(actors);
+        setActors(actors)
+
+      })
+  }, [])
 
 
 
 
   return (
     <>
-     <h1> Actors and Actress list</h1>
-     <div>
-      <ul>
-        {actress.map((actress)=>(
-          <li key={actress.id}>{actress.name}</li>
-        ))}
-      </ul>
-     </div>
+      <h1> Actors and Actress list</h1>
+      <div>
+        <ul>
+          {actress.map((actress) => (
+            <li key={actress.id}>{actress.name}</li>
+          ))}
+        </ul>
+      </div>
     </>
   )
 }
